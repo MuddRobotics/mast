839f87c6333d9fa00d227156783de025aad85ec0
abagaria <abagaria@hmc.edu>
2014-11-30 22:28:22 -0800

    Created MASTs Catkin WS

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/mast/Desktop/MAST/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/mast/Desktop/MAST/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mast/Desktop/MAST/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/mast/Desktop/MAST/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/mast/Desktop/MAST/devel/lib:/home/mast/Desktop/MAST/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/mast/Desktop/MAST/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/mast/Desktop/MAST/devel/lib/pkgconfig:/home/mast/Desktop/MAST/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/mast/Desktop/MAST/build"
+export PYTHONPATH="/home/mast/Desktop/MAST/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mast/Desktop/MAST/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/mast/Desktop/MAST/src:$ROS_PACKAGE_PATH"
